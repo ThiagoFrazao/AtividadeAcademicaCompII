7370f0d89065b958cddc17722293cfdf4292aabc
samara <samara@samara-PC>
2019-07-02 02:02:13 -0300

    Produindo mais instalações na fábrica de instalações e montando o main

--- comp2/src/Zoologico.java
+++ comp2/src/Zoologico.java
@@ -1,9 +1,13 @@
+import animais.Elefante;
 import animais.Leao;
+import animais.Macaco;
 import animais.enums.Porte;
 import animais.enums.Sexo;
 import instalacao.FabricaInstalacao;
 import instalacao.enums.Localizacao;
+import instalacao.tipos.InstalacaoElefante;
 import instalacao.tipos.InstalacaoLeao;
+import instalacao.tipos.InstalacaoMacaco;
 
 public class Zoologico {	
 	public static void main(String[] args) {
@@ -15,6 +19,19 @@ public class Zoologico {
 		instalacaoLeao.instalaAnimal(leoa);
 		instalacaoLeao.instalaAnimal(leao);
 		
+		Elefante elefante = new Elefante(Sexo.MASCULINO, Porte.GRANDE);
+		Elefante elefanta = new Elefante(Sexo.MASCULINO, Porte.GRANDE);
+
+		InstalacaoElefante instalacaoElefante = FabricaInstalacao.criarInstalacaoElefante(Localizacao.OESTE);
+		instalacaoElefante.instalaAnimal(elefante);
+		instalacaoElefante.instalaAnimal(elefanta);
+		
+		Macaco macaco = new Macaco(Sexo.MASCULINO, Porte.PEQUENO);
+		Macaco macaca = new Macaco(Sexo.MASCULINO, Porte.PEQUENO);
+		
+		InstalacaoMacaco instalacaoMacaco = FabricaInstalacao.criarInstalacaoMacaco(Localizacao.OESTE);
+		instalacaoMacaco.instalaAnimal(macaco);
+		instalacaoMacaco.instalaAnimal(macaca);
 		
 		
 		
--- comp2/src/instalacao/FabricaInstalacao.java
+++ comp2/src/instalacao/FabricaInstalacao.java
@@ -1,7 +1,14 @@
 package instalacao;
 
 import instalacao.enums.Localizacao;
+import instalacao.tipos.InstalacaoBaleia;
+import instalacao.tipos.InstalacaoElefante;
+import instalacao.tipos.InstalacaoFoca;
 import instalacao.tipos.InstalacaoLeao;
+import instalacao.tipos.InstalacaoMacaco;
+import instalacao.tipos.InstalacaoPeixeBoi;
+import instalacao.tipos.InstalacaoPinguim;
+import instalacao.tipos.InstalacaoTartaruga;
 import instalacao.tipos.InstalacaoTubarao;
 import instalacao.tipos.Jaula;
 import instalacao.tipos.Tanque;
@@ -22,6 +29,52 @@ public class FabricaInstalacao {
 		return instalacao;
 	}
 	
+	public static InstalacaoElefante criarInstalacaoElefante(Localizacao localizacao) {
+		Jaula jaula = new Jaula(20, 30, 25);
+		InstalacaoElefante instalacao = new InstalacaoElefante(2, localizacao, 23);
+		instalacao.adicionaInstalacao(jaula);
+		return instalacao;
+	}
 	
+	public static InstalacaoMacaco criarInstalacaoMacaco(Localizacao localizacao) {
+		Jaula jaula = new Jaula(40, 35.7, 90);
+		InstalacaoMacaco instalacao = new InstalacaoMacaco(10, localizacao, 25);
+		instalacao.adicionaInstalacao(jaula);
+		return instalacao;
+	}
+	
+	public static InstalacaoTartaruga criarInstalacaoTartaruga(Localizacao localizacao) {
+		Tanque tanque = new Tanque(200);
+		InstalacaoTartaruga instalacao = new InstalacaoTartaruga(17, localizacao, 20);
+		instalacao.adicionaInstalacao(tanque);
+		return instalacao;
+	}
+	
+	public static InstalacaoPinguim criarInstalacaoPinguim(Localizacao localizacao) {
+		Tanque tanque = new Tanque(100);
+		InstalacaoPinguim instalacao = new InstalacaoPinguim(25, localizacao, 15);
+		instalacao.adicionaInstalacao(tanque);
+		return instalacao;
+	}
+	
+	public static InstalacaoFoca criarInstalacaoFoca(Localizacao localizacao) {
+		Tanque tanque = new Tanque(100);
+		InstalacaoFoca instalacao = new InstalacaoFoca(5, localizacao, 15);
+		instalacao.adicionaInstalacao(tanque);
+		return instalacao;
+	}
 
+	public static InstalacaoPeixeBoi criarInstalacaoPeixeBoi(Localizacao localizacao) {
+		Tanque tanque = new Tanque(750);
+		InstalacaoPeixeBoi instalacao = new InstalacaoPeixeBoi(7, localizacao, 18);
+		instalacao.adicionaInstalacao(tanque);
+		return instalacao;
+	}
+	
+	public static InstalacaoBaleia criarInstalacaoBaleia(Localizacao localizacao) {
+		Tanque tanque = new Tanque(1000);
+		InstalacaoBaleia instalacao = new InstalacaoBaleia(19, localizacao, 2);
+		instalacao.adicionaInstalacao(tanque);
+		return instalacao;
+	}
 }
